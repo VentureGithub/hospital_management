11ab74fc1cf81e082c4bd01090f34fb26cf54069
Mahima Chaudhary <sidhumahima2005@gmail.com>
2025-10-07 19:17:33 +0530

    7-10-25

--- src/app/UnauthorisedPage.js/page.js
+++
@@ -1,9 +0,0 @@
-export default function Unauthorized() {
-    return (
-      <div>
-        <h1>Unauthorized</h1>
-        <p>You do not have permission to access this page.</p>
-      </div>
-    );
-  }
-  
